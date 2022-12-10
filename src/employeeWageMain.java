c2d313ef176beffeb756f1e687a2516653f8f563
NAVEEDKHAN88 <md.naveedpathan88@gmail.com>
2022-12-10 19:06:25 +0530

    calculate dailyWage using Switch Case

--- src/employeeWageMain.java
+++ src/employeeWageMain.java
@@ -1,44 +1,41 @@
 public class employeeWageMain {
 
     int WagePerHr = 20;
-    int isFullTime = 1;
-    int isPartTime = 2;
+    final int isFullTime = 1;
+    final int isPartTime = 2;
     int empHr;
     int EmpWage;
 
-    public void attend()
-    {
-        double randomfn = Math.floor(Math.random()*10)%3;
-        if (randomfn == isFullTime)
-        {
-            empHr = 8;
-            System.out.println("Employee Works Fullday");
-        } else if (randomfn == isPartTime)
+    public void attend() {
+        int randomfn = (int) Math.floor(Math.random() * 10) % 3;
+        switch (randomfn) {
+            case isFullTime:
+                empHr = 8;
+                System.out.println("Employee works Fulltime");
+                break;
 
-        {
-            empHr =  4;
-            System.out.println("Employee Works partTime");
+            case isPartTime:
+                empHr = 4;
+                System.out.println("Employee works Parttime");
+                break;
+
+            default:
+                empHr = 0;
+                System.out.println("Employee is Absent");
+
+
+
         }
-
-
-         else
-        {
-            empHr=0;
-            System.out.println("Employee is Absent");
-        }
-    }
-
-    public void calcWage()
-    {
-         EmpWage= empHr * WagePerHr;
+        EmpWage = empHr * WagePerHr;
         System.out.println(EmpWage);
+
     }
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         employeeWageMain emp = new employeeWageMain();
         emp.attend();
-        emp.calcWage();
+
 
     }
-    }
+}
 
