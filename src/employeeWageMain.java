b0f8fe5ca15e4bddd9cd78ef9ada22e25a7af8f4
NAVEEDKHAN88 <md.naveedpathan88@gmail.com>
2022-12-10 18:06:35 +0530

    employe is present or absent

--- src/employeeWageMain.java
+++ src/employeeWageMain.java
@@ -1,5 +1,22 @@
 public class employeeWageMain {
-    public static void main(String[] args) {
-        System.out.println("welcome to Employee Wage Computation Program using oop's");
+    int isPresent = 1;
+
+    public void attend()
+    {
+        double randomfn = Math.floor(Math.random()*10)%2;
+        if (randomfn == isPresent)
+        {
+            System.out.println("Employee is Present");
+        }else
+        {
+            System.out.println("Employee is Absent");
+        }
     }
-}
+    public static void main(String[] args) {
+
+        employeeWageMain emp = new employeeWageMain();
+        emp.attend();
+
+    }
+    }
+
