dea31e271dc5504db5c448a42b409abc69147385
NAVEEDKHAN88 <md.naveedpathan88@gmail.com>
2022-12-10 18:34:00 +0530

    calculate EmployeWage

--- src/employeeWageMain.java
+++ src/employeeWageMain.java
@@ -1,21 +1,34 @@
 public class employeeWageMain {
-    int isPresent = 1;
+
+    int WagePerHr = 20;
+    int isFullTime = 1;
+    int empHr;
+    int EmpWage;
 
     public void attend()
     {
         double randomfn = Math.floor(Math.random()*10)%2;
-        if (randomfn == isPresent)
+        if (randomfn == isFullTime)
         {
-            System.out.println("Employee is Present");
+            empHr = 8;
+            System.out.println("Employee Works Fullday");
         }else
         {
+            empHr=0;
             System.out.println("Employee is Absent");
         }
+    }
+
+    public void calcWage()
+    {
+         EmpWage= empHr * WagePerHr;
+        System.out.println(EmpWage);
     }
     public static void main(String[] args) {
 
         employeeWageMain emp = new employeeWageMain();
         emp.attend();
+        emp.calcWage();
 
     }
     }
